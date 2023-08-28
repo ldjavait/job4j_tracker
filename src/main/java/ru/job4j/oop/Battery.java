e2d5df1d3a628360304b276d9a0ff3f4fbd10dab
D L <lazykindit@yandex.ru>
2023-08-28 15:29:13 +0300

    1.8. Класс использующий сам себя [#167388 #249323]

---
+++ src/main/java/ru/job4j/oop/Battery.java
@@ -0,0 +1,18 @@
+package ru.job4j.oop;
+
+public class Battery {
+    private int load;
+
+    public Battery(int load) {
+        this.load = load;
+    }
+
+    public String about() {
+        return "My charge: " + load + "%";
+    }
+
+    public void exchange(Battery another) {
+        another.load = another.load + this.load;
+        this.load = 0;
+    }
+}

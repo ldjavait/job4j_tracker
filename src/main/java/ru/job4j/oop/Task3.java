3602ea2d8d4fbfd34c0a304a5ad19f3029b94613
D L <lazykindit@yandex.ru>
2023-08-30 15:48:37 +0300

    Мой output

---
+++ src/main/java/ru/job4j/oop/Task3.java
@@ -0,0 +1,6 @@
+package ru.job4j.oop;
+
+public class Task3 {
+    void output(String message) {
+    }
+}
---
+++ src/test/java/ru/job4j/oop/Task3Test.java
@@ -0,0 +1,18 @@
+package ru.job4j.oop;
+
+import org.junit.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class Task3Test {
+    @Test
+    public void checkMainOutput() {
+        var methods = Task3.class.getDeclaredMethods();
+        assertThat(methods.length, is(1));
+        var method = methods[0];
+        assertThat(method.getName(), is("output"));
+        var params = method.getParameters();
+        assertThat(params.length, is(1));
+        assertThat(params[0].getType().getName(), is(String.class.getName()));
+    }
+}

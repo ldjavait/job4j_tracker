0e865f9886d17934384edf79ad21a873e73c7aa2
D L <lazykindit@yandex.ru>
2023-09-04 11:21:54 +0300

    7. Метод удаления заявки Tracker.delete [#211749 #251865]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -91,4 +91,16 @@ public class Tracker {
         }
         return false;
     }
+
+    /**
+     * @param id заявки, которую удаляем.
+     */
+    public void delete(int id) {
+        int index = indexOf(id);
+        if (index != -1) {
+            System.arraycopy(items, index + 1, items, index, size - index - 1);
+            items[size - 1] = null;
+            size--;
+        }
+    }
 }

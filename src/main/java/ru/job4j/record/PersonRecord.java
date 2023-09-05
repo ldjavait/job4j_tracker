6e75618883d006e611a0dababd1ff60c095c408c
D L <lazykindit@yandex.ru>
2023-09-05 09:49:17 +0300

    11. Record [#504880 #375694]. Правки по задаче.

--- src/main/java/ru/job4j/record/PersonRecord.java
+++ src/main/java/ru/job4j/record/PersonRecord.java
@@ -1,4 +1,30 @@
 package ru.job4j.record;
 
-public record PersonRecord(String name, int age) {
+/**
+ * 11. Record [#504880 #375694]
+ *
+ * @param name
+ * @param age
+ */
+public record PersonRecord(String name, int age) implements Comparable<PersonRecord> {
+    public static int maxAge = 100;
+
+    public PersonRecord {
+        if (age > 101) {
+            throw new IllegalArgumentException("Возраст должен быть менее 101");
+        }
+    }
+
+    public static int getMaxAge() {
+        return maxAge;
+    }
+
+    public void info() {
+        System.out.println("Напечатать информацию");
+    }
+
+    @Override
+    public int compareTo(PersonRecord o) {
+        return name.compareTo(o.name);
+    }
 }

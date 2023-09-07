09101a9e512421a8c60e075853b52e80e2f47106
D L <lazykindit@yandex.ru>
2023-09-07 16:56:13 +0300

    4.0. Что такое Interface. [#504946 #376743]

---
+++ src/main/java/ru/job4j/polymorph/Main.java
@@ -0,0 +1,37 @@
+package ru.job4j.polymorph;
+
+/**
+ * 4.0. Что такое Interface. [#504946 #376743]
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("town car");
+        Vehicle townCar = new TownCar();
+        townCar.changeGear();
+        townCar.accelerate();
+        townCar.steer();
+        townCar.brake();
+        System.out.println("sport car");
+        Vehicle sportCar = new SportCar();
+        sportCar.changeGear();
+        sportCar.accelerate();
+        sportCar.steer();
+        sportCar.brake();
+        System.out.println("sport car если реализуем 2 интерфейса");
+        SportCar sportCar1 = new SportCar();
+        Vehicle vehicle = sportCar1;
+        Fuel fuel = sportCar1;
+        vehicle.changeGear();
+        vehicle.accelerate();
+        vehicle.steer();
+        vehicle.brake();
+        fuel.refill();
+        System.out.println();
+        Vehicle townCar1 = new TownCar();
+        townCar1.changeGear();
+        townCar1.accelerate();
+        townCar1.steer();
+        townCar1.brake();
+        townCar1.refill();
+    }
+}

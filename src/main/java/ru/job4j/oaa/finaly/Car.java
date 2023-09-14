cee6958cd02ea12d8ac6d5d3197de65e3869ee52
D L <lazykindit@yandex.ru>
2023-09-14 14:19:33 +0300

    3. final [#334073 #255880]

---
+++ src/main/java/ru/job4j/oaa/finaly/Car.java
@@ -0,0 +1,10 @@
+package ru.job4j.oaa.finaly;
+
+/**
+ * 3. final [#334073 #255880]
+ */
+public class Car {
+    public final void printMark() {
+        System.out.println("Метод выводит марку автомобиля");
+    }
+}
---
+++ src/main/java/ru/job4j/oaa/finaly/Toyota.java
@@ -0,0 +1,7 @@
+package ru.job4j.oaa.finaly;
+
+/**
+ * 3. final [#334073 #255880]
+ */
+public class Toyota extends Car {
+}

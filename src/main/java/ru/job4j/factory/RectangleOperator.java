d64df6c4007d64fc7a8d711a28ddfa00dca59023
D L <lazykindit@yandex.ru>
2023-09-14 08:42:53 +0300

    2. Шаблон фабричный метод [#95067 #255867]

---
+++ src/main/java/ru/job4j/factory/RectangleOperator.java
@@ -0,0 +1,16 @@
+package ru.job4j.factory;
+
+import java.util.Scanner;
+
+public class RectangleOperator extends ShapeOperator {
+    private final Scanner input = new Scanner(System.in);
+
+    @Override
+    public Shape createShape() {
+        System.out.print("Введите ширину прямоугольника: ");
+        int a = Integer.parseInt(input.nextLine());
+        System.out.print("Введите длину прямоугольника: ");
+        int b = Integer.parseInt(input.nextLine());
+        return new Rectangle(a, b);
+    }
+}

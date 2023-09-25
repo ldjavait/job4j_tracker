5dd73f6f348b690625229e588fdec57887863408
D L <lazykindit@yandex.ru>
2023-09-25 08:25:35 +0300

    1. Интерфейс Queue [#504942 #377785]. Правки по задаче.

--- src/main/java/ru/job4j/collection/queue/AppleStore.java
+++ src/main/java/ru/job4j/collection/queue/AppleStore.java
@@ -16,13 +16,13 @@ public class AppleStore {
         for (int i = 0; i < count - 1; i++) {
             queue.poll();
         }
-        return queue.peek().name();
+        return queue.peek() != null ? queue.peek().name() : null;
     }
 
     public String getFirstUpsetCustomer() {
         for (int i = 0; i < count; i++) {
             queue.poll();
         }
-        return queue.peek().name();
+        return queue.peek() != null ? queue.peek().name() : null;
     }
 }

ac7ebca31ca313ec67f217245747571f7d40e803
D L <lazykindit@yandex.ru>
2023-09-27 07:59:48 +0300

    1. Паспорт и жители [#212656 #258738]

---
+++ src/test/java/ru/job4j/collection/citizen/PassportOfficeTest.java
@@ -0,0 +1,15 @@
+package ru.job4j.collection.citizen;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class PassportOfficeTest {
+    @Test
+    public void whenTestAddMethod() {
+        Citizen citizen = new Citizen("2f44a", "Petr Arsentev");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen);
+        assertThat(office.get(citizen.getPassport())).isEqualTo(citizen);
+    }
+}

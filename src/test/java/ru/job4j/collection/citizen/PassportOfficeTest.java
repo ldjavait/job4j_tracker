21e8d51c8238941c85f1faeb43050b75fd0c5822
D L <lazykindit@yandex.ru>
2023-09-27 08:33:33 +0300

    1. Паспорт и жители [#212656 #258738]. Правки по задаче.

--- src/test/java/ru/job4j/collection/citizen/PassportOfficeTest.java
+++ src/test/java/ru/job4j/collection/citizen/PassportOfficeTest.java
@@ -12,4 +12,15 @@ class PassportOfficeTest {
         office.add(citizen);
         assertThat(office.get(citizen.getPassport())).isEqualTo(citizen);
     }
+
+    @Test
+    public void whenNoAddTestMethod() {
+        Citizen citizen = new Citizen("2f44a", "Petr Arsentev");
+        Citizen dup = new Citizen("2f44a", "Petr Arsentev");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen);
+        boolean expected = office.add(dup);
+        assertThat(office.get(citizen.getPassport())).isEqualTo(citizen);
+        assertThat(expected).isFalse();
+    }
 }

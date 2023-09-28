5eb79c359709a6831e423b502ec4b27d123b054b
D L <lazykindit@yandex.ru>
2023-09-28 13:42:23 +0300

    1. Организовать сортировку User [#10034 #259272]

---
+++ src/main/java/ru/job4j/collection/TreeSort.java
@@ -0,0 +1,20 @@
+package ru.job4j.collection;
+
+import java.util.Collections;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class TreeSort {
+    public static void main(String[] args) {
+        Set<Integer> numbers = new TreeSet<>();
+        numbers.add(5);
+        numbers.add(1);
+        numbers.add(3);
+        System.out.println("Сортировка по возрастанию" + numbers);
+        Set<Integer> num = new TreeSet<>(Collections.reverseOrder());
+        num.add(5);
+        num.add(1);
+        num.add(3);
+        System.out.println("Сортировка по возрастанию" + num);
+    }
+}

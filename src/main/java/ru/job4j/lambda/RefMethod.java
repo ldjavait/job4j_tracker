6974f5f8b582f1a05003859dffc6eec325c7d117
D L <lazykindit@yandex.ru>
2023-10-04 08:28:45 +0300

    3. Ссылки на методы [#70608 #366791]

---
+++ src/main/java/ru/job4j/lambda/RefMethod.java
@@ -0,0 +1,28 @@
+package ru.job4j.lambda;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class RefMethod {
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList(
+                "Ivan",
+                "Petr Arsentev"
+        );
+        Consumer<String> consumer = System.out::println;
+        names.forEach(consumer);
+        Comparator<Integer> intCmp = Integer::compare;
+        Consumer<String> out = RefMethod::cutOut;
+        names.forEach(out);
+    }
+
+    public static void cutOut(String value) {
+        if (value.length() > 10) {
+            System.out.println(value.substring(0, 10) + "..");
+        } else {
+            System.out.println(value);
+        }
+    }
+}

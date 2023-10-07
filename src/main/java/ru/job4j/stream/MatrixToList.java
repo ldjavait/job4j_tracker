feb4b90d71ad8fcab2aa9b5c04b03a5f39d9daa9
D L <lazykindit@yandex.ru>
2023-10-07 13:16:29 +0300

    10. Преобразование матрицы чисел в список чисел. FlatMap. [#110228 #368788]. Правки по задаче.

--- src/main/java/ru/job4j/stream/MatrixToList.java
+++ src/main/java/ru/job4j/stream/MatrixToList.java
@@ -5,6 +5,12 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class MatrixToList {
+    /**
+     * Преобразует матрицу из входного параметра в список чисел.
+     *
+     * @param matrix то, что преобразуем.
+     * @return преобразованная матрица.
+     */
     public static List<Integer> convert(Integer[][] matrix) {
         return Arrays.stream(matrix)
                 .flatMap(Arrays::stream)
